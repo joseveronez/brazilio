f159cfda9673729cfd1c041e5b925b55281cad71
joseveronez <jozeveronez@gmail.com>
2017-10-09 13:21:46 -0300

    Ajustes Iniciais
    Exclusão de arquivos desnecessários, e inclusão do modelo php.

---
+++ views/componentes/footer.php
@@ -0,0 +1,30 @@
+<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="container">
+    </div>
+</footer>
+</body>
+</html>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+        
+    });
+
+	function Trim() {
+		var get_value = document.getElementById('telefone');
+		var campo_temp;
+		campo_temp = get_value.value.substring(5,6);
+
+		if(campo_temp == "9" || campo_temp == "8") {
+			/* console.log("mascara mobile"); */
+			$("#telefone").attr("onKeyPress", "MascaraCelular(formulario.telefone);");
+			$("#telefone").attr("onBlur", "ValidaCelular(formulario.telefone);");
+			$("#telefone").attr("maxlength", "16");
+		} else {
+			/* console.log("mascara telefone"); */
+			$("#telefone").attr("onKeyPress", "MascaraTelefone(formulario.telefone);");
+			$("#telefone").attr("onBlur", "ValidaTelefone(formulario.telefone);");
+			$("#telefone").attr("maxlength", "14");
+		}
+	}
+</script>
