4414862d1423df20e39e959c5c21a28ce0655193
joseveronez <jozeveronez@gmail.com>
2017-10-09 16:42:17 -0300

    Footer
    Criação do footer

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -1,5 +1,47 @@
-<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <div class="container">
+<footer>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-dourado">
+        <div class="container MarginT4 MarginB4">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center MarginT4 MarginB4">
+                <h3 class="branco-fonte">CONTATO</h3>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <h5 class="branco-fonte">
+                Entre em contato por telefone ou pelo formulário ao lado:</h5>
+                <h5 class="marrom-fonte">
+                Brazilio Bacellar, Shirai Advogados <br>
+                OAB/PR 997  •  CNPJ 04.510.577/0001-02</h5>
+                <h6 class="branco-fonte">
+                Tel.: +55 41 3352.8363 <br>
+                Fax. +55 41 3352.0167 </h6>
+                <h5 class="branco-fonte">ESCRITÓRIO</h5>
+                <h6 class="branco-fonte">
+                Rua Marechal Hermes, 272 | Centro Cívico <br>
+                Curitiba | Paraná | Brasil | CEP 80.530-230</h6>
+                <h5 class="branco-fonte">ESTACIONAMENTO</h5>
+                <h6 class="branco-fonte">Travessa José do Patrocinio, 48 | Alto da Glória Curitiba <br>
+                    | Paraná | Brasil | CEP 80.030-190</h6>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <form action="" class="form-horizontal Roboto Regular" id="formulario">
+                    <input type="text" class="form-control contato-footer bg-dourado" name="nome" placeholder="NOME" required><br>
+                    <input type="email" class="form-control contato-footer bg-dourado" name="email" placeholder="E-MAIL" required><br>
+                    <input type="text" class="form-control contato-footer bg-dourado" name="telefone" placeholder="TELEFONE" required><br>
+                    <textarea class="form-control contato-footer bg-dourado" name="mensagem" rows="4" cols="3" placeholder="MENSAGEM"></textarea><br>
+                    <button type="submit" class="btn btn-formulario size13 pull-right">ENVIAR</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-azul PaddingT4 PaddingB4">
+        <div class="container">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 bg-azul">
+                <img src="imagens/logobrazilio_dourado.png" style="img-responsive">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 bg-azul">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 bg-azul">
+            </div>
+        </div>
     </div>
 </footer>
 </body>
