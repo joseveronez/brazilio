dd6aea2c874c0045d2fd888f4caf90c879431fc5
joseveronez <jozeveronez@gmail.com>
2017-10-10 09:21:30 -0300

    Home
    Inclusão de textos

--- views/home.php
+++ views/home.php
@@ -1,10 +1,24 @@
+<style type="text/css">
+    .owl-carousel .owl-item img {width: initial;}
+</style>
+
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul" style="margin-bottom: -10px">
     <div class="owl-carousel slider-topo owl-theme">
         <div class="item vh100">
-            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban1.jpg'); background-size: cover; background-position: center center"></div>
+            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban2.jpg'); background-size: cover; background-position: center center">
+                <div class="container">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center MarginT25">
+                        <h4 class="Uppercase size50 dourado-fonte Light">Brazilio Bacellar,<br> SHIRAI Advogados </h4>
+                        <img src="imagens/ond1.png" class="img-responsive MarginT5" style="margin: 0 auto;">
+                        <h6 class="Uppercase branco-fonte Light size20 MarginB4" style="line-height: 28px;">a solução dos conflitos de interesses preventivamente,<br> evitando o desgaste e a morosidade do Poder Judiciário. </h6>
+                        <a href="#" class="link-default dourado-fonte botao-dourado" role="button" >SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                    </div>
+                    
+                </div>
+            </div>
         </div>
         <div class="item vh100">
-            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban2.jpg'); background-size: cover; background-position: center center"></div>
+            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban1.jpg'); background-size: cover; background-position: center center"></div>
         </div>
         <div class="item vh100">
             <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban3.jpg'); background-size: cover; background-position: center center"></div>
@@ -20,7 +34,7 @@
         nav:true,
         navText: ["<div class='prev-icon'></div>","<div class='next-icon'></div>"],
         dots:true,
-        autoplay:true,
+        autoplay:false,
         items:1
     });
 </script>
