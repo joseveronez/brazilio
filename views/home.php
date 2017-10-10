496f16b348bbd465dc85e3f2b878b5db5ac90b62
joseveronez <jozeveronez@gmail.com>
2017-10-10 11:26:42 -0300

    Home
    Ajsutes

--- views/home.php
+++ views/home.php
@@ -12,8 +12,7 @@
                         <img src="imagens/ond1.png" class="img-responsive MarginT5" style="margin: 0 auto;">
                         <h6 class="Uppercase branco-fonte Light size20 MarginB4" style="line-height: 28px;">a solução dos conflitos de interesses preventivamente,<br> evitando o desgaste e a morosidade do Poder Judiciário. </h6>
                         <a href="#" class="link-default dourado-fonte botao-dourado" role="button" >SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
-                    </div>
-                    
+                    </div> 
                 </div>
             </div>
         </div>
@@ -25,7 +24,119 @@
         </div>
     </div>
 </div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-bege PaddingT5 PaddingB5">
+    <div class="container">
+        <img src="imagens/balanca.png" class="img-responsive" style="margin: 0 auto;">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT1 text-center">
+            <h3 class="Medium size30 azul-fonte" style="letter-spacing: 3px;">A EMPRESA</h3>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sm-offset-3 MarginT2">
+        <p class="text-justify size16 Medium">
+            <span class="Bold">A Brazilio Bacellar, Shirai Advogados</span> é uma sociedade profissional experiente, com atuação preventiva e contenciosa nos campos do direito comercial, civil, societário, administrativo, trabalhista e tributário, com enfoque na atividade empresarial. 
+        </p>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center MarginT5">
+            <a href="#" class="link-default azul-fonte botao-azul" role="button" >SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco PaddingT5 PaddingB5">
+    <div class="container">
+        <img src="imagens/martelo.png" class="img-responsive" style="margin: 0 auto;">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT1 text-center">
+            <h3 class="Medium size30 azul-fonte" style="letter-spacing: 3px;">ÁREAS DE ATUAÇÕES</h3>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sm-offset-3 MarginT2">
+            <p class="text-justify size16 Medium">
+                A qualidade dos serviços advocatícios é reforçada pela parceria com profissionais habilitados e em constante atualização, capazes de auxiliar na solução de questões relacionadas às mais diversas áreas do Direito. 
+            </p>
+            <a href="#" class="text-justify Medium dourado-fonte link-default">
+                CONHEÇA NOSSA EQUIPE&nbsp;&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+        </div>
+        
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT3">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 col-xs-offset-1">
+                <img src="imagens/administrativo.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">ADMINISTRATIVO</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/ambiental.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">AMBIENTAL</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/bancario.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">BANCARIO</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/civel.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">CÍVEL</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/comercial.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">COMERCIAL</h5>
+            </div>
+        </div>
+        
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT3">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 col-xs-offset-1">
+                <img src="imagens/consumidor.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">CONSUMIDOR</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/contrato.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">CONTRATOS</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/familias.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">FAMÍLIAS</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/gestao_legal_patrimonio.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">GESTÃO LEGAL DE PATRIMÔNIO</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/internet_e_ecommerce.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">INTERNET E E-COMMERCE</h5>
+            </div>
+        </div>
+        
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT3">
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2 col-xs-offset-1">
+                <img src="imagens/propriedade_intelectual.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">PROPRIEDADE INTELECTUAL</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/societario.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">SOCIETÁRIO</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/sucessoes.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">SUCESSÕES</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/trabalhista.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">TRABALHISTA</h5>
+            </div>
+            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
+                <img src="imagens/tributario.png" class="img-responsive" style="margin: 0 auto;">
+                <h5 class="text-center">TRIBUTÁRIO</h5>
+            </div>
+        </div>
+        
+    </div>
+</div>
 
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco parallax">
+    <div class="container">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT10">
+            <h3 class="text-center branco-fonte Light" style="font-style:italic;">Esteja atualizado sobre os nossos serviços, lançamentos e muito mais!</h3>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT4">
+            <input type="email" class="form-control email-news">
+        </div>
+    </div>
+</div>
 
 <script type="text/javascript">
     $('.owl-carousel.slider-topo').owlCarousel({
