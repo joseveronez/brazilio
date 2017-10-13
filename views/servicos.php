ebba12ebaaef45c07aa9b5e1a0efe44c64a75be0
joseveronez <jozeveronez@gmail.com>
2017-10-13 08:34:28 -0300

    Servicos
    pagina servicos concluída

--- views/servicos.php
+++ views/servicos.php
@@ -21,35 +21,151 @@
     </div>
 </div>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco PaddingT8 Relative">
-    <img src="<?= RAIZSITE ?>/imagens/separador2.png" class="img-responsive Absolute hidden-sm hidden-xs" style=" left: 50%; top: -5%; z-index:1;">
+    <img src="<?= RAIZSITE ?>/imagens/separador2.png" class="img-responsive Absolute hidden-sm hidden-xs" style=" left: 50%; top: -1%; z-index:1;">
     <div class="container">
-        <img src="<?= RAIZSITE ?>/imagens/martelo.png" class="img-responsive" style="margin: 0 auto;">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB5">
+            <img src="<?= RAIZSITE ?>/imagens/martelo.png" class="img-responsive" style="margin: 0 auto;">
+        </div>
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero PaddingT2 PaddingB8">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+            <div class="col-lg-4 col-md-4 col-sm-4 hidden-xs">
                 <ul class="list-unstyled">
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">ADMINISTRATIVO</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">AMBIENTAL</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">BANCÁRIO</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">CÍVEL</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">COMERCIAL</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">COMSUMIDOR</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">CONTRATOS</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">FAMÍLIA</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">GESTÃO LEGAL DE PATRIMÔNIO</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">INTERNET E E-COMMERCE</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">PROPRIEDADE INTELECTUAL</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">SOCIETÁRIO</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">SUCESSÕES</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">TRABALHISTA</a></li>
-                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte">TRIBUTÁRIO</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">ADMINISTRATIVO</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">AMBIENTAL</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">BANCÁRIO</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">CÍVEL</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">COMERCIAL</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">COMSUMIDOR</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">CONTRATOS</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">FAMÍLIA</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">GESTÃO LEGAL DE PATRIMÔNIO</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">INTERNET E E-COMMERCE</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">PROPRIEDADE INTELECTUAL</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">SOCIETÁRIO</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">SUCESSÕES</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">TRABALHISTA</a></li>
+                    <li class="lista-servicos"><a href="#" class="link-default dourado-fonte lista-servicos-item">TRIBUTÁRIO</a></li>
                 </ul>
             </div>
-            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB4">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
                     <img src="<?= RAIZSITE ?>/imagens/administrativo.png" class="img-responsive" style="float: left;">
-                    <h3 class="margin-zero titulo-servicos">ADMINISTRATIVO</h3><br>
-                    <p class="text-justify size15">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;ADMINISTRATIVO</h3><br>
+                    <p class="text-justify size15 texto-servicos">
                         Assessoria nas esferas administrativa e judicial para empresas interessadas na participação em processos licitatórios, efetuando a análise da legalidade dos editais e elaboração de possíveis impugnações aos editais ou propostas dos concorrentes etc., bem como, tomando as medidas cabíveis à cada caso.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/ambiental.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;AMBIENTAL</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                        Orientação e assessoria de empresas acerca da legislação ambiental, atuando de forma preventiva e no contencioso, bem como prestando auxílio na obtenção de licenças/autorização perante os órgãos competentes.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/bancario.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;BANCÁRIO</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                        Assessoria e orientação acerca dos serviços prestados pelas instituições financeiras, e das limitações legais existentes para sua atuação em vista dos diversos diplomas legais aplicáveis à espécie.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/civel.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;cível</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                       A atuação na área cível engloba a tomada de medidas no âmbito preventivo e contencioso, dando suporte na solução de litígios em todas as esferas do direito civil. 
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/comercial.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;comercial</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                       Atividades na área de consultoria e contencioso, buscando alternativas e resolução de questões envolvendo contratos e operações comerciais. 
+                        Em especial na área de recuperação de empresas e falimentar, os anos de experiência possibilitam a consultoria e assessoria jurídica em todas as fases dos processos de falência,concordata e recuperação (judicial e extrajudicial), tanto para o devedor quanto para os credores. 
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/consumidor.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;consumidor</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Suporte jurídico e orientações preventivas acerca da regulamentação das relações entre consumidores e fornecedores de produtos e serviços. 
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/contrato.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;contratos</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Análise, elaboração, revisão e readequação de contratos e assessoria contratual em geral, especialmente no que tange à atividade empresarial, bem como na elaboração de pareceres e opiniões legais, e em havendo necessidade, tomada de todas as medidas legais cabíveis para revisão judicial das cláusulas contratuais.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/familias.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;família</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Atuação no âmbito do direito de família, garantindo a solução mais célere e eficiente para todas as questões pertinentes.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/gestao_legal_patrimonio.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;gestão legal de patrimônio</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Assessoria no âmbito de proteção patrimonial contra contingências diversas e organização de ativos e investimentos.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/internet_e_ecommerce.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;internet e e-commerce</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Assessoria no âmbito de proteção patrimonial contra contingências diversas e organização de ativos e investimentos.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/propriedade_intelectual.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;propriedade intelectual</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Assessoria no âmbito de proteção patrimonial contra contingências diversas e organização de ativos e investimentos.
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/societario.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;societário</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Assessoria jurídica e consultoria na constituição, organização e transformações das sociedades empresariais, bem como elaboração de planejamento societário. Representação em discussões administrativas e judiciais, buscando defender os interesses das sociedades e dos sócios/acionistas. 
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/sucessoes.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;sucessões</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Assessoria jurídica na interposição das medidas necessárias para concretização e encerramento do processo de sucessão (arrolamentos e inventários) bem como orientação jurídica para planejamento sucessório. 
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/trabalhista.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;trabalhista</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      A assessoria jurídica trabalhista é voltada para os interesses patronais, oferecendo consultoria preventiva e atuação no contencioso.  
+                    </p>
+                </div>
+                
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginB7">
+                    <img src="<?= RAIZSITE ?>/imagens/tributario.png" class="img-responsive" style="float: left;">
+                    <h3 class="margin-zero titulo-servicos dourado-fonte Uppercase">&nbsp;&nbsp;&nbsp;tributário</h3><br>
+                    <p class="text-justify size15 texto-servicos">
+                      Atuação na área administrativa, tanto no auxílio em medidas preventivas, como em defesas perante os órgãos arrecadadores de tributos bem como prestando assessoria na obtenção de incentivos fiscais, planejamento tributário e em procedimento de compensação e restituição de tributos. 
+                    No âmbito judicial, destaque para as medidas na recuperação de tributos indevidamente exigidos e recolhidos perante as esferas Municipal, Estadual e Federal. 
                     </p>
                 </div>
             </div>
