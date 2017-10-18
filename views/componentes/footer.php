93ffce9172a3c0a5114b2ef6b0af83b6c231770a
SampaioO <sampaio.anything@gmail.com>
2017-10-18 08:08:03 -0200

    Site
    Iniciando ajustes finos na sessão de contato
    > Inclusas novas cores alternativas

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -24,36 +24,38 @@
     <?php
         if($pagina_atual == "home") {
     ?>
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-dourado Relative">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-dourado Relative contato">
         <img src="imagens/separador.png" class="img-responsive Absolute" style=" left: 50%; top: -9%;">
-        <div class="container MarginT4 MarginB4">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center MarginT4 MarginB4">
-                <h3><i class="fa fa-envelope-o branco-fonte" aria-hidden="true"></i></h3>
-                <h3 class="branco-fonte" style="letter-spacing: 3px;">CONTATO</h3>
+        <div class="container conteudo">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                <h3 class="branco-fonte margin-zero">
+                    <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                </h3>
+                <h3 class="branco-fonte margin-zero" style="letter-spacing: 3px;">CONTATO</h3>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <h5 class="branco-fonte">
-                Entre em contato por telefone ou pelo formulário ao lado:</h5>
-                <h5 class="marrom-fonte">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 dados">
+                <p class="branco-fonte chamada">
+                Entre em contato por telefone ou pelo formulário ao lado:</p>
+                <p class="Bold cnpj Uppercase">
                 Brazilio Bacellar, Shirai Advogados <br>
-                OAB/PR 997  •  CNPJ 04.510.577/0001-02</h5>
-                <h6 class="branco-fonte">
-                Tel.: +55 41 3352.8363 <br>
-                Fax. +55 41 3352.0167 </h6>
-                <h5 class="branco-fonte">ESCRITÓRIO</h5>
-                <h6 class="branco-fonte">
+                OAB/PR 997 • CNPJ 04.510.577/0001-02</p>
+                <p class="branco-fonte">
+                Tel.: <span class="size11">+55 41</span> 3352.8363 <br>
+                Fax. <span class="size11">+55 41</span> 3352.0167 </p>
+                <p class="branco-fonte titulo">ESCRITÓRIO</p>
+                <p class="branco-fonte endereco">
                 Rua Marechal Hermes, 272 | Centro Cívico <br>
-                Curitiba | Paraná | Brasil | CEP 80.530-230</h6>
-                <h5 class="branco-fonte">ESTACIONAMENTO</h5>
-                <h6 class="branco-fonte">Travessa José do Patrocinio, 48 | Alto da Glória Curitiba <br>
-                    | Paraná | Brasil | CEP 80.030-190</h6>
+                Curitiba | Paraná | Brasil | CEP 80.530-230</p>
+                <p class="branco-fonte titulo">ESTACIONAMENTO</p>
+                <p class="branco-fonte endereco">Travessa José do Patrocinio, 48 | Alto da Glória Curitiba <br>
+                    | Paraná | Brasil | CEP 80.030-190</p>
             </div>
             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
                 <form action="" class="form-horizontal Roboto Regular" id="formulario">
-                    <input type="text" class="form-control contato-footer branco-fonte bg-dourado" name="nome" placeholder="NOME" required><br>
-                    <input type="email" class="form-control contato-footer branco-fonte bg-dourado" name="email" placeholder="E-MAIL" required><br>
-                    <input type="text" class="form-control contato-footer branco-fonte bg-dourado" name="telefone" placeholder="TELEFONE" required><br>
-                    <textarea class="form-control contato-footer branco-fonte bg-dourado" name="mensagem" rows="4" cols="3" placeholder="MENSAGEM"></textarea><br>
+                    <input type="text" class="form-control contato-footer branco-alternativo-fonte bg-dourado" name="nome" placeholder="NOME" required><br>
+                    <input type="email" class="form-control contato-footer branco-alternativo-fonte bg-dourado" name="email" placeholder="E-MAIL" required><br>
+                    <input type="text" class="form-control contato-footer branco-alternativo-fonte bg-dourado" name="telefone" placeholder="TELEFONE" required><br>
+                    <textarea class="form-control contato-footer branco-alternativo-fonte bg-dourado" name="mensagem" rows="4" cols="3" placeholder="MENSAGEM"></textarea><br>
                     <button type="submit" class="btn btn-formulario size13 pull-right">ENVIAR</button>
                 </form>
             </div>
