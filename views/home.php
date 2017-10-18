cdf8dda5d34bd100dc24ec27bb06339db1c7c6bb
SampaioO <sampaio.anything@gmail.com>
2017-10-18 10:11:07 -0200

    Site
    Home
    > Ajustes realizados no slider

--- views/home.php
+++ views/home.php
@@ -1,26 +1,43 @@
-<style type="text/css">
-    .owl-carousel .owl-item img {width: initial;}
-</style>
-
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul" style="margin-bottom: -10px">
+<?php
+    ScriptLoader::LoadCSS('home');
+?>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
     <div class="owl-carousel slider-topo owl-theme">
         <div class="item vh100">
             <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban2.jpg'); background-size: cover; background-position: center center; min-height: 481px;">
-                <div class="container">
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center MarginT22">
-                        <h4 class="Uppercase size50 dourado-fonte Light">Brazilio Bacellar,<br> SHIRAI Advogados </h4>
-                        <img src="imagens/ond1.png" class="img-responsive MarginT5" style="margin: 0 auto;">
-                        <h6 class="Uppercase branco-fonte Light size20 MarginB4" style="line-height: 28px;">a solução dos conflitos de interesses preventivamente,<br> evitando o desgaste e a morosidade do Poder Judiciário. </h6>
-                        <a href="#" class="link-default dourado-fonte botao-dourado" role="button">SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
-                    </div> 
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slider-conteudo">
+                    <p class="Uppercase dourado-fonte Light titulo">Brazilio Bacellar,<br> SHIRAI Advogados</p>
+
+                    <img src="imagens/ond1.png" class="img-responsive">
+                    <p class="Uppercase branco-fonte Light subtitulo">a solução dos conflitos de interesses preventivamente,<br> evitando o desgaste e a morosidade do Poder Judiciário.</p>
+
+                    <a href="#" class="link-default botao-dourado" role="button">SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
                 </div>
             </div>
         </div>
         <div class="item vh100">
-            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban1.jpg'); background-size: cover; background-position: center center;  min-height: 481px;"></div>
+            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban1.jpg'); background-size: cover; background-position: center center;  min-height: 481px;">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slider-conteudo">
+                    <p class="Uppercase dourado-fonte Light titulo">Brazilio Bacellar,<br> SHIRAI Advogados</p>
+
+                    <img src="imagens/ond1.png" class="img-responsive">
+                    <p class="Uppercase branco-fonte Light subtitulo">a solução dos conflitos de interesses preventivamente,<br> evitando o desgaste e a morosidade do Poder Judiciário.</p>
+
+                    <a href="#" class="link-default botao-dourado" role="button">SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
         </div>
         <div class="item vh100">
-            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban3.jpg'); background-size: cover; background-position: center center;  min-height: 481px;"></div>
+            <div class="vh100" style="background: url('<?= RAIZSITE ?>/imagens/ban3.jpg'); background-size: cover; background-position: center center;  min-height: 481px;">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center slider-conteudo">
+                    <p class="Uppercase dourado-fonte Light titulo">Brazilio Bacellar,<br> SHIRAI Advogados</p>
+
+                    <img src="imagens/ond1.png" class="img-responsive">
+                    <p class="Uppercase branco-fonte Light subtitulo">a solução dos conflitos de interesses preventivamente,<br> evitando o desgaste e a morosidade do Poder Judiciário.</p>
+
+                    <a href="#" class="link-default botao-dourado" role="button">SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
