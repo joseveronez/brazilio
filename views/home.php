4e96278821fc440b6bc4cdd5535a450967735356
SampaioO <sampaio.anything@gmail.com>
2017-10-18 11:34:35 -0200

    Site
    Ajustes finos realizados na home
    > Ajustes realizados no slider.
    > Inclusão de links nos botões.
    > Inclusão de hovers na sessão de áreas de atuação.
    > Ajustes de alinhamento.
    > Melhoria no responsivo.

--- views/home.php
+++ views/home.php
@@ -41,110 +41,149 @@
         </div>
     </div>
 </div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-bege PaddingT10 PaddingB10 Relative" id="waypointMenu">
-    <img src="<?= RAIZSITE ?>/imagens/separador2.png" class="img-responsive Absolute hidden-xs" style=" left: 50%; top: -10%; z-index:1;">
-    <div class="container">
-        <img src="<?= RAIZSITE ?>/imagens/balanca.png" class="img-responsive" style="margin: 0 auto;">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT1 text-center">
-            <h3 class="Medium size30 azul-fonte" style="letter-spacing: 3px;">A EMPRESA</h3>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 empresa padding-zero bg-bege" id="waypointMenu">
+    <div class="linha-marrom"></div>
+    <div class="container conteudo">
+        <img src="<?= RAIZSITE ?>/imagens/balanca.png" class="img-responsive">
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center titulo">
+            <h3 class="Medium size30 azul-fonte">A EMPRESA</h3>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sm-offset-3 MarginT2">
-        <p class="text-justify size16 Medium">
-            <span class="Bold">A Brazilio Bacellar, Shirai Advogados</span> é uma sociedade profissional experiente, com atuação preventiva e contenciosa nos campos do direito comercial, civil, societário, administrativo, trabalhista e tributário, com enfoque na atividade empresarial. 
-        </p>
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+        <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12 descritivo">
+            <p class="text-justify size16 Medium">
+                <span class="Bold">A Brazilio Bacellar, Shirai Advogados</span> é uma sociedade profissional experiente, com atuação preventiva e contenciosa nos campos do direito comercial, civil, societário, administrativo, trabalhista e tributário, com enfoque na atividade empresarial. 
+            </p>
         </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center MarginT5">
-            <a href="#" class="link-default azul-fonte botao-azul" role="button" >SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+            <a href="<?= RAIZSITE ?>/sobre" class="link-default botao-azul" role="button">SAIBA MAIS&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i></a>
         </div>
     </div>
+    <img src="<?= RAIZSITE ?>/imagens/ond2.png" class="img-responsive Absolute hidden-sm hidden-xs">
 </div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco PaddingT10 PaddingB10 Relative">
-    <img src="<?= RAIZSITE ?>/imagens/ond2.png" class="img-responsive Absolute hidden-sm hidden-xs" style=" left: 60%; top: -3%; z-index:1;">
-    <img src="<?= RAIZSITE ?>/imagens/separador2.png" class="img-responsive Absolute hidden-xs" style=" left: 50%; top: -5%;">
-    <div class="container">
-        <img src="<?= RAIZSITE ?>/imagens/martelo.png" class="img-responsive" style="margin: 0 auto;">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT1 text-center">
-            <h3 class="Medium size30 azul-fonte" style="letter-spacing: 3px;">ÁREAS DE ATUAÇÕES</h3>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 areas-atuacao padding-zero bg-branco">
+    <div class="linha-marrom"></div>
+    <div class="container conteudo">
+        <img src="<?= RAIZSITE ?>/imagens/martelo.png" class="img-responsive">
+        
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center titulo">
+            <h3 class="Medium size30 azul-fonte">ÁREAS DE ATUAÇÕES</h3>
         </div>
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sm-offset-3 MarginT2">
-            <p class="text-justify size16 Medium">
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+        <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12 descritivo">
+            <p class="text-justify size16 Medium margin-zero">
                 A qualidade dos serviços advocatícios é reforçada pela parceria com profissionais habilitados e em constante atualização, capazes de auxiliar na solução de questões relacionadas às mais diversas áreas do Direito. 
-            </p>
-            <a href="#" class="text-justify Medium dourado-fonte link-default">
+            </p><br>
+            <a href="<?= RAIZSITE ?>/equipe" class="Medium link-default equipe-link">
                 CONHEÇA NOSSA EQUIPE&nbsp;&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>
             </a>
         </div>
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
         
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT10">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
-                <div class="col-lg-3 col-md- col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/administrativo.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">ADMINISTRATIVO</h5>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 lista-itens">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero itens">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/administrativo.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">ADMINISTRATIVO&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/ambiental.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">AMBIENTAL</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/ambiental.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">AMBIENTAL&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/bancario.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">BANCARIO</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/bancario.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">BANCARIO&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/civel.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">CÍVEL</h5>
-                </div>
-            </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT5">
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/comercial.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">COMERCIAL</h5>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/consumidor.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">CONSUMIDOR</h5>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/contrato.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">CONTRATOS</h5>
-                </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/familias.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">FAMÍLIAS</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/civel.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">CÍVEL&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT5">
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/gestao_legal_patrimonio.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">GESTÃO LEGAL DE PATRIMÔNIO</h5>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero itens">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/comercial.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">COMERCIAL&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/internet_e_ecommerce.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">INTERNET E E-COMMERCE</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/consumidor.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">CONSUMIDOR&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/propriedade_intelectual.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">PROPRIEDADE INTELECTUAL</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/contrato.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">CONTRATOS&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/societario.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">SOCIETÁRIO</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/familias.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">FAMÍLIAS&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT5">
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/sucessoes.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">SUCESSÕES</h5>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero itens">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/gestao_legal_patrimonio.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">GESTÃO LEGAL DE PATRIMÔNIO&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/trabalhista.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">TRABALHISTA</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/internet_e_ecommerce.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">INTERNET E E-COMMERCE&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
-                    <img src="<?= RAIZSITE ?>/imagens/tributario.png" class="img-responsive" style="margin: 0 auto;">
-                    <h5 class="text-center MarginT10">TRIBUTÁRIO</h5>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/propriedade_intelectual.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">PROPRIEDADE INTELECTUAL&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
                 </div>
-            </div>  
-            
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/societario.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">SOCIETÁRIO&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
+                </div>
+            </div>
+
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero itens">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/sucessoes.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">SUCESSÕES&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/trabalhista.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">TRABALHISTA&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 item">
+                    <img src="<?= RAIZSITE ?>/imagens/tributario.png" class="img-responsive">
+                    <a href="#" class="link-default item-link">
+                        <h5 class="text-center MarginT10 Bold">TRIBUTÁRIO&nbsp;&nbsp;<span><i class="fa fa-chevron-right" aria-hidden="true"></i></span></h5>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
