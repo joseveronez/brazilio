749198bf7092a0a34c0da2b01d49e613b1552468
Nícolas Sampaio <sampaio.anything@gmail.com>
2017-10-19 16:30:05 -0200

    Site
    Ajustes finos realizados na página empresa.
    > Ajustes no banner.
    > Alinhamento de conteudo.
    > Entre linhas.
    > Tamanho de fontes.
    > Inclusão da imagem da onda no canto inferior direito.
    > Inclusão do plugin Fancy Box, para abrir a galeria de imagens.

--- views/sobre.php
+++ views/sobre.php
@@ -1,103 +1,120 @@
+<?php
+    ScriptLoader::LoadCSS('sobre');
+?>
 <style type="text/css">
-    .parallax-sobre { background-image: url("<?= RAIZSITE ?>/imagens/ban2.jpg"); background-position: center center; background-attachment: fixed; background-repeat: no-repeat; background-size: cover;}
+    .parallax-banner { background-image: url("<?= RAIZSITE ?>/imagens/banner-servicos.jpg"); background-position: center center; background-attachment: fixed; background-repeat: no-repeat; background-size: cover; }
+    .prev-icon { background: url(<?= RAIZSITE ?>/imagens/left.png); background-size: 20%; background-position: center center; background-repeat: no-repeat; height: 50px; width: 50px; }
+    .next-icon { background: url(<?= RAIZSITE ?>/imagens/right.png); background-size: 20%; background-position: center center; background-repeat: no-repeat; height: 50px; width: 50px; }
 </style>
 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul" style="margin-bottom: -10px">
-    <div class="item vh30">
-        <div class="vh30 parallax-sobre" style="min-height: 481px;">
-            <div class="container">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center " style="margin: 180px 0px">
-                    <h4 class="Uppercase size35 dourado-fonte Light">A EMPRESA</h4>
-                </div>
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginB1">
-                    <h6 class="branco-fonte">VOCÊ ESTÁ EM HOME / <span class="dourado-fonte">A EMPRESA</span></h6>
-                </div>
+    <div class="parallax-banner">
+        <div class="container">
+            <div class="text-center titulo">
+                <h3 class="Uppercase size35 dourado-fonte Light">A EMPRESA</h3>
+            </div>
+            <div class="breadcrumb hidden-xs">
+                <p class="branco-fonte margin-zero">VOCÊ ESTÁ EM <a href="<?= RAIZSITE ?>" class="link-default">HOME</a> / <span class="dourado-fonte">A EMPRESA</span></p>
             </div>
         </div>
     </div>
 </div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco PaddingT15p Relative">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco PaddingT12p">
     <div id="waypointMenu" style="position: absolute; left: 0; height: 101px; width: 50px; top: -101px"></div>
     <div class="linha"></div>
     <div class="container">
-        <img src="<?= RAIZSITE ?>/imagens/balanca.png" class="img-responsive" style="margin: 0 auto;">
-        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 col-sm-offset-3 MarginT10p padding-zero">
-        <p class="text-justify size16 Medium">
-            A Brazilio Bacellar, Shirai Advogados é uma sociedade profissional experiente, com atuação preventiva e contenciosa nos campos do direito comercial, civil, societário, administrativo, trabalhista e tributário, com enfoque na atividade empresarial. O escritório investe constantemente em atualização técnica e busca sempre novas tecnologias seguras a fim de facilitar e agilizar a prestação de serviços aos seus clientes.
-        </p>
-        <p class="text-justify size16 Medium">
-            A qualidade dos serviços advocatícios é reforçada pela parceria com profissionais habilitados e em constante atualização, capazes de auxiliar na solução de questões relacionadas às mais diversas áreas do Direito. Além disso, os profissionais do escritório possuem conhecimento em línguas estrangeiras e podem atender empresas nacionais e internacionais.
-        </p>
-        <p class="text-justify size16 Medium">
-            A localização da sede é estratégica: no Centro Cívico, próxima aos principais órgãos do Poder Judiciário do Estado do Paraná. Uma ampla estrutura com instalações que atendem todas as necessidades de seus colaboradores e clientes.  
-        </p>
+        <img src="<?= RAIZSITE ?>/imagens/balanca.png" class="img-responsive">
+        
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+        <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12 MarginT10p">
+            <p class="text-justify size14 letter-spacing1 Medium">
+                <strong>A Brazilio Bacellar, Shirai Advogados</strong> é uma sociedade profissional experiente, com atuação preventiva e contenciosa nos campos do direito comercial, civil, societário, administrativo, trabalhista e tributário, com enfoque na atividade empresarial. O escritório investe constantemente em atualização técnica e busca sempre novas tecnologias seguras a fim de facilitar e agilizar a prestação de serviços aos seus clientes.
+            </p><br>
+            <p class="text-justify size14 letter-spacing1 Medium">
+                A qualidade dos serviços advocatícios é reforçada pela parceria com profissionais habilitados e em constante atualização, capazes de auxiliar na solução de questões relacionadas às mais diversas áreas do Direito. Além disso, os profissionais do escritório possuem conhecimento em línguas estrangeiras e podem atender empresas nacionais e internacionais.
+            </p><br>
+            <p class="text-justify size14 letter-spacing1 Medium">
+                A localização da sede é estratégica: no Centro Cívico, próxima aos principais órgãos do Poder Judiciário do Estado do Paraná. Uma ampla estrutura com instalações que atendem todas as necessidades de seus colaboradores e clientes.  
+            </p>
         </div>
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
     </div>
 </div>
-<div class="container">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero bg-branco MarginT10p">
-        <div class="col-lg-4 col-md-5 col-sm-6 col-xs-12 col-md-offset-1 bg-dourado chamada-galeria">
-            <h4 class="branco-fonte Uppercase size22" style="line-height: 35px;">“uma sociedade profissional com atuação preventiva e contenciosa nas mais diversas áreas [...]”</h4>
-        </div>
-        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-8 padding-zero">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul">
-                    <div class="owl-carousel slider-topo owl-theme" id="owl2">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="background: url('<?= RAIZSITE ?>/imagens/galeria1.jpg'); background-size: cover; background-position: center center; height: 200px;"></div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
+    <div class="container galeria">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-branco MarginT10p">
+            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 bg-dourado chamada-galeria">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <h4 class="branco-fonte Uppercase">“uma sociedade profissional com atuação preventiva e contenciosa nas mais diversas áreas [...]”</h4>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 padding-zero">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul">
+                        <div class="owl-carousel slider-topo owl-theme" id="owl2">
+                            <a data-fancybox="galeria1" href="<?= RAIZSITE ?>/imagens/galeria1.jpg">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="background: url('<?= RAIZSITE ?>/imagens/galeria1.jpg'); background-size: cover; background-position: center center; height: 200px;"></div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul">
+                        <div class="owl-carousel slider-topo owl-theme" id="owl3">
+                            <a data-fancybox="galeria2" href="<?= RAIZSITE ?>/imagens/galeria2.jpg">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="background: url('<?= RAIZSITE ?>/imagens/galeria2.jpg'); background-size: cover; background-position: center center; height: 130px;"></div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul">
-                    <div class="owl-carousel slider-topo owl-theme" id="owl3">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="background: url('<?= RAIZSITE ?>/imagens/galeria2.jpg'); background-size: cover; background-position: center center; height: 130px;"></div>
+            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 padding-zero">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul">
+                        <div class="owl-carousel slider-topo owl-theme" id="owl4">
+                            <a data-fancybox="galeria3" href="<?= RAIZSITE ?>/imagens/galeria3.jpg">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="background: url('<?= RAIZSITE ?>/imagens/galeria3.jpg'); background-size: cover; background-position: center center; height: 330px;"></div>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 padding-zero">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="azul">
-                    <div class="owl-carousel slider-topo owl-theme" id="owl4">
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" style="background: url('<?= RAIZSITE ?>/imagens/galeria3.jpg'); background-size: cover; background-position: center center; height: 330px;"></div>
-                    </div>
-                </div>
-            </div>
+
+        <img src="<?= RAIZSITE ?>/imagens/onda-escura.png" class="img-responsive Absolute hidden-sm hidden-xs">
+    </div>
+</div>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
+    <div class="container diferenciais">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center padding-zero">
+            <h3 class="dourado-fonte Light size40 margin-zero">NOSSOS DIFERENCIAIS</h3>
         </div>
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+        <div class="col-lg-6 col-md-8 col-sm-10 col-xs-12 text-left padding-zero Medium diferenciais-lista">
+            <p>Esses são os diferenciais que sua empresa terá ao contratar nosso escritório:</p>
+            <ul class="lista">
+                <li class="MarginT4p"><span class="preto-fonte">Equipe multidisciplinar</span></li>
+                <li class="MarginT4p"><span class="preto-fonte">Atuação pautada pela ética e transparência</span></li>
+                <li class="MarginT4p"><span class="preto-fonte">Atendimento customizado às necessidades do cliente</span></li>
+                <li class="MarginT4p"><span class="preto-fonte">Interlocução direta e constante com os sócios do escritório</span></li>
+                <li class="MarginT4p"><span class="preto-fonte">Vasta experiência no Direito Empresarial</span></li>
+                <li class="MarginT4p"><span class="preto-fonte">Transparência total através de uma página eletrônica com todas as informações dos processos</span></li>
+                <li class="MarginT4p"><span class="preto-fonte">Escritórios parceiros em todo o Brasil</span></li>
+            </ul>
+
+            <a href="<?= RAIZSITE ?>/servicos" class="Medium link-default equipe-link">
+                CONHEÇA NOSSOS SERVIÇOS&nbsp;&nbsp;<i class="fa fa-angle-right" aria-hidden="true"></i>
+            </a>
+        </div>
+        <div class="col-lg-3 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
     </div>
 </div>
 
-<div class="container MarginT5 MarginB8">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center padding-zero">
-        <h3 class="dourado-fonte Light MarginT1p size40">NOSSOS DIFERENCIAIS</h3>
-    </div>
-    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-sm-offset-3 text-left padding-zero Medium MarginT2">
-        <p>Esses são os diferenciais que sua empresa terá ao contratar nosso escritório:</p>
-        <ul class="lista">
-            <li class="MarginT5p"><span class="preto-fonte">Equipe multidisciplinar</span></li>
-            <li class="MarginT5p"><span class="preto-fonte">Atuação pautada pela ética e transparência</span></li>
-            <li class="MarginT5p"><span class="preto-fonte">Atendimento customizado às necessidades do cliente</span></li>
-            <li class="MarginT5p"><span class="preto-fonte">Interlocução direta e constante com os sócios do escritório</span></li>
-            <li class="MarginT5p"><span class="preto-fonte">Vasta experiência no Direito Empresarial</span></li>
-            <li class="MarginT5p"><span class="preto-fonte">Transparência total através de uma página eletrônica com todas as informações dos processos</span></li>
-            <li class="MarginT5p"><span class="preto-fonte">Escritórios parceiros em todo o Brasil</span></li>
-        </ul>
-    </div>
-    <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 col-sm-offset-3 padding-zero MarginT2">
-        <h6 style="letter-spacing: 3px;"><a href="#" class="link-default dourado-fonte" role="button">CONHEÇA NOSSOS SERVIÇOS &nbsp; <i class="fa fa-angle-right" aria-hidden="true"></i></a></h6>
-    </div>
-</div>
-
+<?php
+    ScriptLoader::LoadPLUGINSCSS('fancybox-master/dist/jquery.fancybox.min.css');
+    ScriptLoader::LoadPLUGINSJS('fancybox-master/dist/jquery.fancybox.min.js');
+?>
 
 <script type="text/javascript">
-    $('#owl1').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        navText: ["<div class='prev-icon'></div>","<div class='next-icon'></div>"],
-        dots:false,
-        autoplay:false,
-        items:1
-    });
     $('#owl2').owlCarousel({
         loop:true,
         nav:true,
